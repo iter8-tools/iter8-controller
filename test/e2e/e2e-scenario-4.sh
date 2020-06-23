3be4b19e7950a31af09035f20345ab6cf49e8c38
Hai Huang <huang195@gmail.com>
2020-06-22 20:19:46 -0400

    detect telemetry version for scenario 4

--- test/e2e/e2e-scenario-4.sh
+++ test/e2e/e2e-scenario-4.sh
@@ -3,13 +3,36 @@
 # Exit on error
 #set -e
 
+ISTIO_NAMESPACE=istio-system
+
 DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1; pwd -P )"
 source "$DIR/library.sh"
 
 header "Scenario 4"
 
+echo "Istio namespace: $ISTIO_NAMESPACE"
+MIXER_DISABLED=`kubectl -n $ISTIO_NAMESPACE get cm istio -o json | jq .data.mesh | grep -o 'disableMixerHttpReports: [A-Za-z]\+' | cut -d ' ' -f2`
+ISTIO_VERSION=`kubectl -n $ISTIO_NAMESPACE get pods -o yaml | grep "image:" | grep proxy | head -n 1 | awk -F: '{print $3}'`
+
+if [ -z "$ISTIO_VERSION" ]; then
+  echo "Cannot detect Istio version, aborting..."
+  exit 1
+elif [ -z "$MIXER_DISABLED" ]; then
+  echo "Cannot detect Istio telemetry version, aborting..."
+  exit 1
+fi
+
+echo "Istio version: $ISTIO_VERSION"
+echo "Istio mixer disabled: $MIXER_DISABLED"
+
 header "Create Iter8 Custom Metric"
-kubectl apply -n iter8 -f $DIR/../../doc/tutorials/istio/bookinfo/iter8_metrics_extended.yaml
+if [ "$MIXER_DISABLED" = "false" ]; then
+  echo "Using Istio telemetry v1"
+  kubectl apply -n iter8 -f $DIR/../../doc/tutorials/istio/bookinfo/iter8_metrics_extended.yaml
+else
+  echo "Using Istio telemetry v2"
+  kubectl apply -n iter8 -f $DIR/../../doc/tutorials/istio/bookinfo/iter8_metrics_extended_telemetry-v2.yaml
+fi
 kubectl get configmap iter8config-metrics -n iter8 -oyaml
 
 header "Create Iter8 Experiment"
