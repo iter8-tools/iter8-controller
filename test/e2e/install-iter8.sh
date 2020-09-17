17be20267eb06e289ffc6cfc9347d3245037da90
Michael Kalantar <kalantar@us.ibm.com>
2020-09-17 15:59:30 -0400

    fix call to semver

--- test/e2e/install-iter8.sh
+++ test/e2e/install-iter8.sh
@@ -33,7 +33,7 @@ ISTIO_TELEMETRY="v2"
 if [ "$MIXER_DISABLED" = "false" ]; then
   ISTIO_TELEMETRY="v1"
   PROMETHEUS_JOB_LABEL="istio-mesh"
-elif (( -1 == ${DIR}/../../hack/semver.sh ${ISTIO_VERSION} "1.7.0" )); then
+elif (( -1 == ${DIR}/../../hack/semver.sh ${ISTIO_VERSION} 1.7.0 )); then
   PROMETHEUS_JOB_LABEL="envoy-stat"
 else
   PROMETHEUS_JOB_LABEL="kubernetes-job"
