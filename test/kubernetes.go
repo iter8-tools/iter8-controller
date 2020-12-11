26aff9f16377687c5924681c90ef8f8771b305a7
Michael Kalantar <kalantar@us.ibm.com>
2020-12-11 13:29:48 -0500

    fix build problem for test case

--- test/kubernetes.go
+++ test/kubernetes.go
@@ -15,9 +15,6 @@ limitations under the License.
 package test
 
 import (
-	"fmt"
-	"k8s.io/apimachinery/pkg/runtime"
-
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
